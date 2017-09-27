b7e8eb5514b161271ff9d480d72c30efae7ed96a
Gleb Starkov <glebstarkov@gmail.com>
2017-09-27 19:47:23 +0800

    Project init

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="jumbotron">
+        <div class="media">
+            <div class="media-left">
+                <img class="media-object my-photo" src="./i/gal.jpg" alt="Моё фото">
+            </div>
+            <div class="media-body">
+                <h2 class="media-heading">Всем привет!</h2>
+                <p>Меня зовут Миша. Я живу в г. Улан-Удэ.<br/>Я решил вести свой блог</p>
+            </div>
+        </div>
+    </div>
+@endsection
